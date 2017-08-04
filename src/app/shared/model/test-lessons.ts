55389c57744de8131bd9154152646d50a9e10d69
Marcos <marhanes21@gmail.com>
2017-08-04 16:24:08 +0100

    section 2, lesson 16

---
+++ src/app/shared/model/test-lessons.ts
@@ -0,0 +1,19 @@
+import {Lesson} from "./lesson";
+
+export const testLessons: Lesson[] = [
+  {
+    id: 1,
+    description: 'Setting up an Angular Development Environment',
+    duration: '5:00'
+  },
+  {
+    id: 2,
+    description: 'Running the Lessons Code',
+    duration: '5:00'
+  },
+  {
+    id: 1,
+    description: 'Build Your firt app - hello world step by step',
+    duration: '5:00'
+  }
+]
